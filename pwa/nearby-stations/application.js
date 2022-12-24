bd76f86fa06a24fd16947ee16662c4a70ffcf787
taba9404 <121328121+taba9404@users.noreply.github.com>
2022-12-25 08:57:34 +0900

    Delete pwa/nearby-stations directory

--- pwa/nearby-stations/application.js
+++
@@ -1,6 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/pwa/nearby-stations/service_worker.js')
-        .then(reg => {
-        console.log('Service worker registered!', reg);
-    });
-}
