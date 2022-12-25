b45e01b8c6735a5cd30480fbb773b0d223ac1838
taba9404 <121328121+taba9404@users.noreply.github.com>
2022-12-25 10:29:49 +0900

    Add files via upload

--- pwa/nearby-stations/application.js
+++ pwa/nearby-stations/application.js
@@ -1,5 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/track/pwa/nearby-stations/service_worker.js')
         .then(reg => {
+        console.log('Service worker registered!', reg);
     });
 }
