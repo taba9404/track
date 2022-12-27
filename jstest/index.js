531302f04bbdc48ebf6623c15cead3d8f1b7d2dd
taba9404 <121328121+taba9404@users.noreply.github.com>
2022-12-27 23:42:24 +0900

    Update index.js

--- jstest/index.js
+++ jstest/index.js
@@ -1,5 +1,5 @@
 async function callApi() {
-  const res = fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await res.json();
   console.log(users)
 };
