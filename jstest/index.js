3e0a6351516c9e61396fb1eb35ac07a4972481d6
taba9404 <121328121+taba9404@users.noreply.github.com>
2022-12-28 14:26:08 +0900

    Update index.js

--- jstest/index.js
+++ jstest/index.js
@@ -1,8 +1,7 @@
 async function callApi() {
   //const res = await fetch("https://jsonplaceholder.typicode.com/users");
   //const res = await fetch("https://hokkaidoshimbun.sharepoint.com/teams/taba/Lists/TEST01/AllItems.aspx");  
-  const res = await fetch("https://hokkaidoshimbun.sharepoint.com/:l:/t/taba/FJh0GYj4T3JBnV8IsTdGKI0Biqxd_69ARUFC86OkCAr3Xw?email=taba9404%40gmail.com&e=a3eDxj/_api/web/");
-
+  const res = await fetch("https://hokkaidoshimbun.sharepoint.com/teams/taba/_api/web/lists/getbytitle('TEST01')/Items(3)");
   const users = await res.json();
   console.log(users)
 };
