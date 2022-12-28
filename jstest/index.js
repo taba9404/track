47b1321662a000efc94f13ad2eed9557a34794e9
taba9404 <121328121+taba9404@users.noreply.github.com>
2022-12-28 13:14:39 +0900

    Update index.js

--- jstest/index.js
+++ jstest/index.js
@@ -1,5 +1,6 @@
 async function callApi() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  //const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://hokkaidoshimbun.sharepoint.com/teams/taba/Lists/TEST01/AllItems.aspx");
   const users = await res.json();
   console.log(users)
 };
